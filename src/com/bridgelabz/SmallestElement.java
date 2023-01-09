707c9bb7fb261a4df50884b4d99444c0dec9ec95
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-09 15:48:31 +0530

    Day 3 Array Assignment

---
+++ src/com/bridgelabz/SmallestElement.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = new int[]{25, 5, 7, 75, 56};
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        System.out.println("Smallest Element Present In Given Array => " + min);
+    }
+}
